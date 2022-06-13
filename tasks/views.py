2f30665628f7fba60502ef7015fc27a1e9b53870
Manav Kalra <hellomanav13@gmail.com>
2022-06-13 19:07:02 +0530

    changing runtime

--- tasks/views.py
+++ tasks/views.py
@@ -6,20 +6,32 @@ from .forms import *
 import os,random
 from pathlib import Path
 from django.templatetags.static import static
+from django.conf import settings
 from django.contrib.staticfiles.utils import get_files
 from django.contrib.staticfiles.storage import StaticFilesStorage
 
 
 
 def index(request):
-    print(request)
+    # print(request)
     # path = static('tasks/images')
     # print(path)
     s = StaticFilesStorage()
-    img_list=list(get_files(s, location=os.path.join('tasks','images')))
+    print('sstatic path is')
+    print(s.base_url)
+    print('static root is')
+    print(settings.STATIC_ROOT)
+    print('directories')
+    img_dir=os.path.join(settings.STATIC_ROOT,"tasks","images")
+    img_list=os.listdir(img_dir)
+    # print(img_list)
+    # img_list=list(get_files(s, location=os.path.join('tasks','images')))
+    # img_list=list(get_files(os.path(settings.STATIC_ROOT), location=os.path.join('tasks','images')))
     image_url=''
     try:
-        image_url = random.choice(img_list).split('\\')[2]
+        # image_url = os.path.join(img_dir, random.choice(img_list))
+        img_name=str(random.choice(img_list))
+        image_url=str(img_dir)+ str(img_name)
     except Exception as e:
         print(str(e))
     print(image_url)
@@ -32,7 +44,7 @@ def index(request):
         if form.is_valid():
             form.save()
         return redirect('/')
-    context = {'tasks':tasks, 'form':form,'image_url':image_url}
+    context = {'tasks':tasks, 'form':form,'img_url':img_name}
     return render(request, 'tasks/list.html', context)
 
 def updateTask(request, pk):
