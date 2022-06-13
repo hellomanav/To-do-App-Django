b0c5adf03da3a3a52f425bdfb364fef9d125ce3e
Manav Kalra <hellomanav13@gmail.com>
2022-06-13 15:33:02 +0530

    updated settings

--- todo/settings.py
+++ todo/settings.py
@@ -121,11 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static/",
-    os.path.join(BASE_DIR, "static")
 ]
 MEDIA_URL = '/images/'
 
